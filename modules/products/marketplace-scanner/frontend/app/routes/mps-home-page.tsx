ec098f8b5a7fde0f11f4ca73672d7f576dce5827
viqueen <hasnae@viqueen.org>
2023-06-14 12:26:30 +1000

    LABSET-37: welcome profile required frontend context provider

--- modules/products/marketplace-scanner/frontend/app/routes/mps-home-page.tsx
+++ modules/products/marketplace-scanner/frontend/app/routes/mps-home-page.tsx
@@ -1,7 +1,12 @@
+import { ProfileRequired } from "@labset-platform-frontend-core/profile-required-context-provider";
 import React from "react";
 
 const MpsHomePage = () => {
-  return <h1>home page</h1>;
+  return (
+    <ProfileRequired>
+      <h1>home page</h1>
+    </ProfileRequired>
+  );
 };
 
 export { MpsHomePage };
