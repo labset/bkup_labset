a755e8798934d13804f3bda6c82d10d26334ff68
viqueen <hasnae@viqueen.org>
2023-06-14 14:21:53 +1000

    LABSET-40: welcome esbuild, also remove unused core tenant tables

--- modules/products/marketplace-scanner/backend/configuration/index.ts
+++ modules/products/marketplace-scanner/backend/configuration/index.ts
@@ -7,7 +7,7 @@ interface MpsConfiguration {
 
 const gatewayUrl = () => {
     if (isLocalstack()) {
-        return `https://zkvffrq9ol.execute-api.localhost.localstack.cloud:4566/prod/labset-gateway/mps`;
+        return `https://zgq56yzlza.execute-api.localhost.localstack.cloud:4566/prod/labset-gateway/mps`;
     }
     return 'http://localhost:4000/labset-gateway/mps';
 };
