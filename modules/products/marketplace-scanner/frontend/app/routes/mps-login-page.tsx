75f78724a3f6ab71e1c8e033728f9cc6512b6fe1
viqueen <hasnae@viqueen.org>
2023-06-14 12:36:02 +1000

    LABSET-39: welcome mps login page

--- modules/products/marketplace-scanner/frontend/app/routes/mps-login-page.tsx
+++ modules/products/marketplace-scanner/frontend/app/routes/mps-login-page.tsx
@@ -1,7 +1,42 @@
+import { mpsGateway } from "@labset-mps-frontend/mps-gateway-component";
+import GoogleIcon from "@mui/icons-material/Google";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  Stack,
+  Typography,
+} from "@mui/material";
 import React from "react";
 
 const MpsLoginPage = () => {
-  return <h1>login page</h1>;
+  return (
+    <Container component="main" maxWidth="xs" sx={{ mt: 4 }}>
+      <Stack spacing={3}>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography variant="h5">Atlassian Marketplace Scanner</Typography>
+        </Box>
+        <Button
+          variant="contained"
+          startIcon={<GoogleIcon />}
+          href={`${mpsGateway.url}/auth/google`}
+        >
+          Continue with google
+        </Button>
+      </Stack>
+    </Container>
+  );
 };
 
 export { MpsLoginPage };
