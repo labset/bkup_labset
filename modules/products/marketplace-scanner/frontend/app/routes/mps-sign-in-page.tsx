37e806c6ae186cf26f915df28c0c4cf23405b485
viqueen <hasnae@viqueen.org>
2023-06-14 13:24:05 +1000

    LABSET-39: welcome sign in page

---
+++ modules/products/marketplace-scanner/frontend/app/routes/mps-sign-in-page.tsx
@@ -0,0 +1,26 @@
+import { useAuthentication } from "@labset-platform-frontend-core/authentication-context-provider";
+import { CircularProgress } from "@mui/material";
+import React, { useEffect } from "react";
+import { Navigate, useSearchParams } from "react-router-dom";
+
+const MpsSignInPage = () => {
+  const [searchParams] = useSearchParams();
+  const { status, actions } = useAuthentication();
+
+  useEffect(() => {
+    const claim = searchParams.get("token");
+    if (claim && status === "loading") {
+      actions.signIn(claim);
+    }
+  }, [status]);
+
+  return (
+    <>
+      {status === "loading" && <CircularProgress color="secondary" />}
+      {status === "not-authenticated" && <Navigate to="/" />}
+      {status === "authenticated" && <Navigate to="/home" />}
+    </>
+  );
+};
+
+export { MpsSignInPage };
