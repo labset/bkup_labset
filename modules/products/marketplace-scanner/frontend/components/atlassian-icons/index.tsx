0030ec2f09b26e947cb2f6476be7758401613811
viqueen <hasnae@viqueen.org>
2023-06-16 19:10:28 +1000

    LABSET-53: fix up icons

--- modules/products/marketplace-scanner/frontend/components/atlassian-icons/index.tsx
+++ modules/products/marketplace-scanner/frontend/components/atlassian-icons/index.tsx
@@ -10,14 +10,24 @@ import JiraLogo from "./jira.svg";
 import OpsGenieLogo from "./opsgenie.svg";
 import StatusPageLogo from "./statuspage.svg";
 
-const BambooIcon = () => <SvgIcon component={BambooLogo} />;
-const BitbucketIcon = () => <SvgIcon component={BitbucketLogo} />;
-const ConfluenceIcon = () => <SvgIcon component={ConfluenceLogo} />;
-const CrowdIcon = () => <SvgIcon component={CrowdLogo} />;
-const FisheyeIcon = () => <SvgIcon component={FisheyeLogo} />;
-const JiraIcon = () => <SvgIcon component={JiraLogo} />;
-const OpsGenieIcon = () => <SvgIcon component={OpsGenieLogo} />;
-const StatusPageIcon = () => <SvgIcon component={StatusPageLogo} />;
+const BambooIcon = () => <SvgIcon component={BambooLogo} viewBox="0 0 32 32" />;
+const BitbucketIcon = () => (
+  <SvgIcon component={BitbucketLogo} viewBox="0 0 32 32" />
+);
+const ConfluenceIcon = () => (
+  <SvgIcon component={ConfluenceLogo} viewBox="0 0 32 32" />
+);
+const CrowdIcon = () => <SvgIcon component={CrowdLogo} viewBox="0 0 32 32" />;
+const FisheyeIcon = () => (
+  <SvgIcon component={FisheyeLogo} viewBox="0 0 32 32" />
+);
+const JiraIcon = () => <SvgIcon component={JiraLogo} viewBox="0 0 32 32" />;
+const OpsGenieIcon = () => (
+  <SvgIcon component={OpsGenieLogo} viewBox="0 0 32 32" />
+);
+const StatusPageIcon = () => (
+  <SvgIcon component={StatusPageLogo} viewBox="0 0 32 32" />
+);
 
 export {
   BambooIcon,
