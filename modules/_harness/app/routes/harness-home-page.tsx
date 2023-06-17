0cf493a99e865e879587289cfcb0fda499908844
viqueen <hasnae@viqueen.org>
2023-06-17 20:33:25 +1000

    LABSET-59: welcome mongodb express icon in harness

--- modules/_harness/app/routes/harness-home-page.tsx
+++ modules/_harness/app/routes/harness-home-page.tsx
@@ -1,3 +1,4 @@
+import { MongoDbIcon } from "@labset-platform-frontend-core/brand-icons-component";
 import { Layout } from "@labset-platform-frontend-core/layout-component";
 import HiveIcon from "@mui/icons-material/Hive";
 import SecurityIcon from "@mui/icons-material/Security";
@@ -8,6 +9,11 @@ import { ProductCard } from "./../components";
 const HarnessHomePage = () => {
   return (
     <Layout topNav={{ Logo: HiveIcon, productName: "Harness", namespaces: [] }}>
+      <ProductCard
+        productTitle={"MongoDB Express"}
+        productUrl={"http://localhost:8081"}
+        Logo={MongoDbIcon}
+      />
       <ProductCard
         Logo={SecurityIcon}
         productTitle={"Atlassian Marketplace Scanner"}
