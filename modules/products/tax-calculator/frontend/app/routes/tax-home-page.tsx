07339d815ba2133f72980ca76ace993c2ff04b33
viqueen <hasnae@viqueen.org>
2023-06-19 14:10:45 +1000

    LABSET-66: welcome initial au tax calculator component

--- modules/products/tax-calculator/frontend/app/routes/tax-home-page.tsx
+++ modules/products/tax-calculator/frontend/app/routes/tax-home-page.tsx
@@ -1,12 +1,18 @@
 import { ProfileRequired } from "@labset-platform-frontend-core/profile-required-context-provider";
+import { AuTaxCalculator } from "@labset-tax-frontend/au-tax-calculator-component";
 import { TaxProductLayout } from "@labset-tax-frontend/tax-product-layout-component";
+import { Grid } from "@mui/material";
 import React from "react";
 
 const TaxHomePage = () => {
   return (
     <ProfileRequired>
       <TaxProductLayout>
-        <h1>tax home page</h1>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <AuTaxCalculator />
+          </Grid>
+        </Grid>
       </TaxProductLayout>
     </ProfileRequired>
   );
---
+++ modules/products/tax-calculator/frontend/components/au-tax-calculator/index.tsx
@@ -0,0 +1,14 @@
+import { Paper } from "@mui/material";
+import React from "react";
+
+import "./au-tax-calculator.css";
+
+const AuTaxCalculator = () => {
+  return (
+    <Paper elevation={3}>
+      <div className="au-tax-calculator">here</div>
+    </Paper>
+  );
+};
+
+export { AuTaxCalculator };
