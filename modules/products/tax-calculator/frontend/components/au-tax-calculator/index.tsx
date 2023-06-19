6f58b2d606e986092b15d9a54cc5039216612b5b
viqueen <hasnae@viqueen.org>
2023-06-19 16:37:32 +1000

    LABSET-66: welcome initial au tax calculator component

--- modules/products/tax-calculator/frontend/components/au-tax-calculator/index.tsx
+++ modules/products/tax-calculator/frontend/components/au-tax-calculator/index.tsx
@@ -1,12 +1,24 @@
-import { Paper } from "@mui/material";
+import { FormControl, Paper, Stack, TextField } from "@mui/material";
 import React from "react";
 
 import "./au-tax-calculator.css";
 
-const AuTaxCalculator = () => {
+const AuTaxCalculator = ({ title }: { title: string }) => {
   return (
     <Paper elevation={3}>
-      <div className="au-tax-calculator">here</div>
+      <div className="au-tax-calculator">
+        <FormControl fullWidth={true}>
+          <Stack spacing={2}>
+            <b>{title}</b>
+            <TextField
+              id="yearly-income"
+              type="number"
+              placeholder="yearly base income"
+              fullWidth={true}
+            />
+          </Stack>
+        </FormControl>
+      </div>
     </Paper>
   );
 };
